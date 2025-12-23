6e2668181559424c3b8a12e884c1f6a06677958e
FershoUno <fersh0un0@gmail.com>
2025-12-23 00:06:31 -0400

    actualizado

--- menu.sh
+++ menu.sh
@@ -1,63 +1,77 @@
-
 #!/bin/bash
-home=$(pwd)
+
+PID_NAVIDAD="/tmp/nucleolinux-navidad.pid"
+PID_ANIONUEVO="/tmp/nucleolinux-anionuevo.pid"
+
+home="$(pwd)"
 chmod +x *.sh
 
-navidad(){
-
-terminator -e "$home/navidad.sh"  > /dev/null 2>&1 &
-return
+navidad() {
+    terminator -f --title="NLBOL" -M -e "$home/navidad.sh" >/dev/null 2>&1 &
+    echo $! > "$PID_NAVIDAD"
 }
 
-an(){
-
-terminator -e "$home/an.sh" > /dev/null 2>&1 &
-return
+an() {
+    terminator -f --title="NLBOL" -M -e "$home/an.sh" >/dev/null 2>&1 &
+    echo $! > "$PID_ANIONUEVO"
 }
 
-detener(){
+detener() {
 
-pkill terminator
-pkill navidad.sh
-pkill an.sh
-pkill audio-loop.sh
-pkill paplay
+    # ---- NAVIDAD ----
+    if [ -f "$PID_NAVIDAD" ]; then
+        echo "Deteniendo Navidad..."
+        killall -15 navidad.sh 2>/dev/null
+        killall -15 audio-loop.sh 2>/dev/null
+        killall -15 paplay 2>/dev/null
+        pkill -f "$home/navidad.sh" 2>/dev/null
+	pkill -15 -f "python3 .*terminator --title=NLBOL"
+        rm -f "$PID_NAVIDAD"
+    fi
+
+    # ---- AÑO NUEVO ----
+    if [ -f "$PID_ANIONUEVO" ]; then
+        echo "Deteniendo Año Nuevo..."
+        killall -15 an.sh 2>/dev/null
+        killall -15 audio-loop.sh 2>/dev/null
+        killall -15 paplay 2>/dev/null
+        pkill -f "$home/an.sh" 2>/dev/null
+        pkill -15 -f "python3 .*terminator --title=NLBOL"
+        rm -f "$PID_ANIONUEVO"
+    fi
 }
 
-salir(){
-exit
+salir() {
+    detener
+    clear
+    exit 0
 }
 
-main(){
-clear
-echo -e '
-	MENU
+main() {
+    clear
+    cat <<EOF
+=========================
+          MENU
+=========================
 
 1.- Navidad
 2.- Detener
 3.- Feliz Año Nuevo
 4.- Salir
 
-'
-echo -n "elegir opcion: "
-read op
-echo 
-echo
-case $op in
+EOF
 
-	1)
-		 navidad;;	
-	2)
-		 detener;;
-	3)
-		an;;
-	4)
-		salir;;
+    read -rp "Elegir opción: " option
 
-esac
+    case "$option" in
+        1) navidad ;;
+        2) detener ;;
+        3) an ;;
+        4) salir ;;
+        *) echo "Opción inválida"; sleep 1 ;;
+    esac
 }
 
-while true
-do
-main
+while true; do
+    main
 done
--- navidad.sh
+++ navidad.sh
@@ -1,75 +1,95 @@
 #!/bin/bash
 
-trap "tput reset; tput cnorm; exit" 2
-nohup ./audio-loop.sh > /dev/null 2>&1 &
-trap "pkill audio-loop.sh && pkill paplay" EXIT
+cleanup() {
+    tput reset
+    tput cnorm
+    if [ -n "$AUDIO_PID" ]; then
+        kill "$AUDIO_PID" 2>/dev/null
+        pkill -P "$AUDIO_PID" 2>/dev/null
+    fi
+}
+
+trap cleanup EXIT INT TERM
+
 clear
 tput civis
+
+./audio-loop.sh >/dev/null 2>&1 &
+AUDIO_PID=$!
+
 lin=2
-col=$(($(tput cols) / 2))
-c=$((col-1))
-est=$((c-2))
+cols=$(tput cols)
+center=$((cols / 2))
+c=$((center - 1))
 color=0
-tput setaf 2; tput bold
 
-# Tree
-for ((i=1; i<20; i+=2))
-{
-    tput cup $lin $col
-    for ((j=1; j<=i; j++))
-    {
-        echo -n \*
-    }
-    let lin++
-    let col--
-}
+tput setaf 2
+tput bold
 
-tput sgr0; tput setaf 3
+for ((i=1; i<20; i+=2)); do
+    tput cup "$lin" "$((center - i/2))"
+    printf '%*s' "$i" '' | tr ' ' '*'
+    ((lin++))
+done
 
-# Trunk
-for ((i=1; i<=2; i++))
-{
-    tput cup $((lin++)) $c
-    echo 'mWm'
-}
-new_year=$(date +'%Y')
-let new_year++
-tput setaf 1; tput bold
-tput cup $lin $((c - 14)); echo FELIZ NAVIDAD NUCLEO LINUX BOLIVIA
-tput cup $((lin + 1)) $((c - 9)); echo Y mucho CODIGO el $new_year
-let c++
+tput sgr0
+tput setaf 3
+
+for ((i=0; i<2; i++)); do
+    tput cup "$((lin+i))" "$((center - 1))"
+    echo "mWm"
+done
+lin=$((lin + 2))
+
+new_year=$(( $(date +'%Y') + 1 ))
+
+msg1="FELIZ NAVIDAD NUCLEO LINUX BOLIVIA"
+msg2="Y mucho CODIGO el $new_year"
+
+tput setaf 1
+tput bold
+tput cup "$lin"     "$((center - ${#msg1}/2))"; echo "$msg1"
+tput cup "$((lin+1))" "$((center - ${#msg2}/2))"; echo "$msg2"
+
+((lin += 2))
+
+declare -A line column
 k=1
 
-# Lights and decorations
 while true; do
-    for ((i=1; i<=35; i++)) {
+    for ((i=1; i<=35; i++)); do
 
-        # Turn off the lights
-        [ $k -gt 1 ] && {
-            tput setaf 2; tput bold
-            tput cup ${line[$[k-1]$i]} {column[$[k-1]$i]}; echo \*
-            unset line[$[k-1]$i]; unset column[$[k-1]$i]  # Array cleanup
-        }
+        if (( k > 1 )); then
+            prev=$((k-1))
+            tput setaf 2
+            tput bold
+            tput cup "${line[$prev,$i]}" "${column[$prev,$i]}"
+            echo "*"
+            unset line["$prev,$i"]
+            unset column["$prev,$i"]
+        fi
 
         li=$((RANDOM % 9 + 3))
-        start=$((c-li+2))
-        co=$((RANDOM % (li-2) * 2 + 1 + start))
-        tput setaf $color; tput bold   # Switch colors
-        tput cup $li $co
-        echo o
-        line[$k$i]=$li
-        column[$k$i]=$co
-        color=$(((color+1)%8))
-        # Flashing text
-        sh=1
-        for l in C O D I G O
-        do
-            tput cup $((lin+1)) $((c-3+sh))
-            echo $l
-            let sh++
-            sleep 0.01
+        half=$((li - 1))
+        co=$((center - half + RANDOM % (li * 2)))
 
+        tput setaf "$color"
+        tput bold
+        tput cup "$li" "$co"
+        echo "o"
+
+        line["$k,$i"]=$li
+        column["$k,$i"]=$co
+
+        color=$(((color + 1) % 8))
+
+        sh=1
+        for l in C O D I G O; do
+            tput cup "$((lin))" "$((center - 3 + sh))"
+            echo "$l"
+            ((sh++))
+            sleep 0.01
         done
-    }
+    done
     k=$((k % 2 + 1))
 done
